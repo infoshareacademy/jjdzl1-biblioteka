0629a65a4f0db9110b4a829673c96ca95923bd35
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2018-12-04 15:02:48 +0100

    JZL1BI-22 - add Employee class

---
+++ src/java/com/infoshareacademy.library/Employee.java
@@ -0,0 +1,69 @@
+package infoshareacademy.library;
+
+public class Employee {
+    private int id;                 //  unikatowy nr pracownika
+    private String userName;        // login pracownika
+    private String password;        // hasło do logowania(może się przyda może nie)
+    private String fullName;        // Pełne imię i nazwisko pracownika
+    private boolean active;         // czy pracownik jest aktywny
+
+    // konstruktor
+
+    public Employee(int id, String userName, String password, String fullName, boolean active) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+        this.active = active;
+    }
+
+    // gettery i settery
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void pringEmployee() {
+        System.out.println("Id pracownika: " + id +
+                " , Login: " + userName +
+                " , Imię i nazwisko: " + fullName +
+                " , Pracownik aktywny: " + active);
+    }
+
+}
--- src/java/com/infoshareacademy.library/Main.java
+++ src/java/com/infoshareacademy.library/Main.java
@@ -1,8 +1,15 @@
 package com.infoshareacademy.library;
 
+import infoshareacademy.library.Employee;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Library !");
+
+        // przykładowy pracownik jeszcze nie pobierany z pliku zewnętrznego
+        // hasło nie jest wyswietalne
+        Employee jank = new Employee(1, "jank", "11111", "Jan Kowalski", true);
+        jank.pringEmployee();
     }
 }
