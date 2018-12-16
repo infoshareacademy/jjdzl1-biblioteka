8a94c18d389f576d46a66755bbfe55d98f02474a
Slawomir Trojniak <sly@troyniak.com>
2018-12-16 18:59:37 +0100

    Zmodyfikowany projekt - zaimportowany plik mavena

---
+++ src/main/java/com/infoshareacademy/library/BookImportCSV.java
@@ -0,0 +1,10 @@
+package com.infoshareacademy.library;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface BookImportCSV {
+
+    List bookObjectCreate() throws IOException;
+
+}
---
+++ src/main/java/com/infoshareacademy/library/Main.java
@@ -0,0 +1,14 @@
+package com.infoshareacademy.library;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Library !");
+
+        // przykładowy pracownik jeszcze nie pobierany z pliku zewnętrznego
+        // hasło nie jest wyswietalne
+        Employee jank = new Employee(1, "jank", "11111", "Jan Kowalski", true);
+        jank.printEmployee();
+
+    }
+}
