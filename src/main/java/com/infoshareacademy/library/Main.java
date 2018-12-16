e365155eab44330daae282dac09ed4519feeb616
Slawomir Trojniak <sly@troyniak.com>
2018-12-16 19:18:48 +0100

    JZL1Bl-32

--- src/main/java/com/infoshareacademy/library/Main.java
+++ src/main/java/com/infoshareacademy/library/Main.java
@@ -5,10 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Library !");
 
-        // przykładowy pracownik jeszcze nie pobierany z pliku zewnętrznego
-        // hasło nie jest wyswietalne
-        Employee jank = new Employee(1, "jank", "11111", "Jan Kowalski", true);
-        jank.printEmployee();
+
 
     }
 }
