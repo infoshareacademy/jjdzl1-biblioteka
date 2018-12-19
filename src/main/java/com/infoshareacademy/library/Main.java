0a74c73c87ab4abd5fbd745ff526c5244a809054
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2018-12-19 16:56:18 +0100

    A few more main class corections

--- src/main/java/com/infoshareacademy/library/Main.java
+++ src/main/java/com/infoshareacademy/library/Main.java
@@ -1,30 +1,33 @@
 package com.infoshareacademy.library;
 
+
 public class Main {
 
     public static void main(String[] args) {
         header();
         if (MenuForImportData.fileIsLoaded == 0) {
             MenuForImportData.returnPathToFile();
+
+            // po wczytaniu danych fileIsLoaded ustawione jest na 1
+            // tak aby wiecej ta opcja nie pojawiła sie przy uruchaminiu menu
+            MenuForImportData.fileIsLoaded = 1;
         }
-        MenuForImportData.fileIsLoaded = 1;
-        System.out.println(MenuForImportData.fileIsLoaded);
         if (MenuForImportData.fileIsLoaded == 1) {
             header();
             System.out.println("Wczytano plik z danymi");
             System.out.println();
+
             // Funkcjonalnść poniżej jeszcze nie działa
             System.out.println("1. Przegladanie listy książek");
             System.out.println("2. Rezerwacja książki");
             System.out.println("3. Wypożyczenie ksiązki");
+
         }
     }
 
     public static void header() {
         System.out.println("-----------------------------------------------------------------------------");
-        System.out.println("|                                                                           |");
         System.out.println("|                        Witaj w aplikacji Bibliteka                        |");
-        System.out.println("|                                                                           |");
         System.out.println("-----------------------------------------------------------------------------");
 
     }
