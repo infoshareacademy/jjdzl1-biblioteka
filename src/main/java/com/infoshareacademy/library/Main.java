ba84d316281cdd377f79fd1efd7b93fbe469b12c
Trojniak Slawomir <sly@troyniak.com>
2019-01-02 21:41:54 +0100

    Main.java - tiny joke added

--- src/main/java/com/infoshareacademy/library/Main.java
+++ src/main/java/com/infoshareacademy/library/Main.java
@@ -45,7 +45,7 @@ public class Main {
 
     public static void header() {
         System.out.println("-----------------------------------------------------------------------------");
-        System.out.println("|                        Witaj w aplikacji Bibliteka                        |");
+        System.out.println("|                 Witaj w aplikacji Bibliteka by Arkadius                   |");
         System.out.println("-----------------------------------------------------------------------------");
 
     }
