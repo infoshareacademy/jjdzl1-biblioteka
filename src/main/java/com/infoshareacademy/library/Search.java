c415c6e59a138bc64fd976de89403018683b4eb6
Trojniak Slawomir <sly@troyniak.com>
2019-01-02 21:28:38 +0100

    Tiny corrections.

--- src/main/java/com/infoshareacademy/library/Search.java
+++ src/main/java/com/infoshareacademy/library/Search.java
@@ -11,15 +11,15 @@ public class Search {
     public static void searchBook(List<Book> ksiazki) throws IOException {
 
         System.out.println("Wpisz tytuł książki:");
-        Scanner tytul = new Scanner(System.in);
-        String wpisanytytul = tytul.nextLine().toLowerCase();
+        Scanner title = new Scanner(System.in);
+        String inputTitle = title.nextLine().toLowerCase();
 
         System.out.println("Wyniki wyszukiwania:");
 
 
         for (int i = 0; i < MenuForImportData.listaKsiazek.size(); i++) {
             String newTitleBook = MenuForImportData.listaKsiazek.get(i).toString().toLowerCase();
-            if (newTitleBook.contains(wpisanytytul)) {
+            if (newTitleBook.contains(inputTitle)) {
                 System.out.println(MenuForImportData.listaKsiazek.get(i));
             }
         }
